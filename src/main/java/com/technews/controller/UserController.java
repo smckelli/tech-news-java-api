00b7824d224f7dd3b39327f97c667026096b450c
smckelli <smckelli@gmail.com>
2022-10-03 16:27:20 -0500

    controllers added

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -14,58 +14,58 @@ import java.util.List;
 
 @RestController
 public class UserController {
-}
 
-@Autowired
-UserRepository repository;
+    @Autowired
+    UserRepository repository;
 
-@Autowired
-VoteRepository voteRepository;
+    @Autowired
+    VoteRepository voteRepository;
 
-@GetMapping("/api/users")
-public List<User> getAllUsers() {
-    List<User> userList = repository.findAll();
-    for (User u : userList) {
-        List<Post> postList = u.getPosts();
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        for (User u : userList) {
+            List<Post> postList = u.getPosts();
+            for (Post p : postList) {
+                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            }
+        }
+        return userList;
+    }
+
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Integer id) {
+        User returnUser = repository.getById(id);
+        List<Post> postList = returnUser.getPosts();
         for (Post p : postList) {
             p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
         }
-    }
-    return userList;
-}
 
-@GetMapping("/api/users/{id}")
-public User getUserById(@PathVariable Integer id) {
-    User returnUser = repository.getById(id);
-    List<Post> postList = returnUser.getPosts();
-    for (Post p : postList) {
-        p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+        return returnUser;
     }
 
-    return returnUser;
-}
-
-@PostMapping("/api/users")
-public User addUser(@RequestBody User user) {
-    // Encrypt password
-    user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-    repository.save(user);
-    return user;
-}
-
-@PutMapping("/api/users/{id}")
-public User updateUser(@PathVariable int id, @RequestBody User user) {
-    User tempUser = repository.getById(id);
-
-    if (!tempUser.equals(null)) {
-        user.setId(tempUser.getId());
+    @PostMapping("/api/users")
+    public User addUser(@RequestBody User user) {
+        // Encrypt password
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
         repository.save(user);
+        return user;
     }
-    return user;
-}
 
-@DeleteMapping("/api/users/{id}")
-@ResponseStatus(HttpStatus.NO_CONTENT)
-public void deleteUser(@PathVariable int id) {
-    repository.deleteById(id);
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        User tempUser = repository.getById(id);
+
+        if (!tempUser.equals(null)) {
+            user.setId(tempUser.getId());
+            repository.save(user);
+        }
+        return user;
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id) {
+        repository.deleteById(id);
+    }
 }
